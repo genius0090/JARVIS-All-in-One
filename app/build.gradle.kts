7a24e392f3f632bc6b1afbc67c06dcb9ffedb0f2
genius0090 <deepakm19980@gmail.com>
2026-09-22 04:57:50 +0530

    Fix formatting of compileOptions in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,21 +2,25 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
+
 android {
     namespace = "com.jarvis.allinone"
     compileSdk = 36
+
     defaultConfig {
         applicationId = "com.jarvis.allinone"
         minSdk = 26
         targetSdk = 36
-compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
         versionCode = 1
         versionName = "1.0.0"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 }
+
 dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.activity:activity-compose:1.10.1")
